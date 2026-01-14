86c3229c6f4bd872022160128f4770a4f8450e75
mz038197 <mz038197@gmail.com>
2026-01-15 01:15:53 +0800

    Update setup.bat to improve clarity of setup steps and messages

--- setup.bat
+++ setup.bat
@@ -1,19 +1,19 @@
 @echo off
-echo ======= 準備建立課程基本環境與模組 =======
+echo ======= Setup Python Environment =======
 echo.
 
-echo [1/3] 建立Python虛擬環境...
+echo [1/3] Install Python...
 if not exist venv (
     py -3 -m venv venv
 )
 
-echo [2/3] 啟動Python虛擬環境中...
+echo [2/3] Activate Python Environment...
 call venv\Scripts\activate
-echo [2/3] 啟動Python虛擬環境完成! 
+echo [2/3] Activate Python Environment! 
 
-echo [3/3] 安裝Python模組中... (過程將花費1分鐘左右)
+echo [3/3] Install Python Packages... (Optional: Install all packages)
 pip install -r requirements.txt
-echo [3/3] 安裝Python模組完成! 請用 VS Code 打開此資料夾進行課程嚕!
+echo [3/3] Install Python Packages! Ready to use VS Code!
 
 pause
 exit /b
